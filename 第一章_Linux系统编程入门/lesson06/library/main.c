2b8923ec37908454ef8d7c90de5c453ea9f4974b
Gabriel-creater <loren39798@gmail.com>
2022-04-14 16:22:06 +0800

    使用动态库需将其加入环境变量

--- 第一章_Linux系统编程入门/lesson06/library/main.c
+++ 第一章_Linux系统编程入门/lesson06/library/main.c
@@ -2,6 +2,7 @@
 //gcc main.c -o app -I ./include
 //调用指定位置的库进行编译，命令如下
 //gcc main.c -o app -I ./include -l calc -L./lib(其中calc为库名，而libcalc.a为库的文件名)
+//注意动态链接库使用时候要将其添加进环境变量，系统变量（/etc/profile）或者用户变量(~/.bashrc)均可
 #include <stdio.h>
 #include "head.h"
 
